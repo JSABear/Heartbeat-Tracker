83969efbd3ed5b5f05a3202ed6b4fb6ac019ac59
santtujk <santtu.karhu@metropolia.fi>
2023-05-04 22:20:27 +0000

    Replace collector.py

--- OS/collector.py
+++ OS/collector.py
@@ -17,14 +17,16 @@ def read(tid):
      
 tmr = Piotimer(freq=250, mode=Piotimer.PERIODIC, callback=read)
 
+bpm_list = []
 l = []
 utime.sleep(0.25)
 ppi = 0
-ppi_list = [1000]
 bpm_int = 0
 bpm = 0
 prev_bpm = 0
 enter = True
+ppi_list = [1000]
+bpm_avg = 0
 
 def make_ppi():
     global ppi, ppi_list
@@ -36,23 +38,28 @@ def make_ppi():
     return ppi_list
 
 def bpm_calc():
-    global bpm, bpm_int
-    bpm = 0
+    global bpm, bpm_int, ppi_list, bpm_list, bpm_avg
+    
     if len(l) > 2:
         ppi = l[-1] - l[-2]
-        bpm = 60/(ppi/1000) # equation to determine bpm 
-        bpm_int = int(bpm)
-    bpm_calc_screen(bpm_int)
-    print(l)
-    return bpm_int
+        if ppi > 500 and ppi < 1500:
+            if ppi != ppi_list[-1]:
+                ppi_list.append(ppi)
+                bpm = 60/(ppi/1000) # equation to determine bpm 
+            bpm_int = int(bpm)
+            bpm_list.append(bpm_int)
+            bpm_avg = round(sum(bpm_list)/len(bpm_list))
+            bpm_calc_screen(bpm_int)
+    #print(l)
+    return bpm_avg, ppi_list
 
 def bpm_calc_screen(value):
     global prev_bpm, enter
     if prev_bpm != value:
-        oled.clear_control(f"{prev_bpm}", 50, 25)
-        oled.display_control(f"{value} BPM", 50, 25)
+        oled.clear_control(f"{prev_bpm}", 45, 15)
+        oled.display_control(f"{value}  BPM", 45, 15)
     if enter == True:
-        oled.display_control("  Collecting...", 5, 35)
+        oled.display_control("  Collecting...", 5, 30)
         oled.display_fill(74, 49, 48, 15)
         oled.clear_control("Cancel", 75, 53)
         enter = False
@@ -60,7 +67,9 @@ def bpm_calc_screen(value):
     return
 
 def collect_data(run_time):
-    global samples, avg, avg_samples, l, beats
+    global samples, avg, avg_samples, l, beats, bpm_list
+    l = []
+    bpm_list = []
     start_time = time.time()
     while True:
         current_time = time.time()
@@ -83,6 +92,7 @@ def collect_data(run_time):
         result = bpm_calc()
         
         if current_time - start_time >= run_time:
+            l = []
             return result
       
 
