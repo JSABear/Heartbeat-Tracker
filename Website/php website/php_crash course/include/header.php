33a3952ea6191faa72002445623436aaaefcc38f
santtujk <santtu.karhu@metropolia.fi>
2023-05-05 19:15:34 +0000

    Upload New File

---
+++ Website/php website/php_crash course/include/header.php
@@ -0,0 +1,37 @@
+<?php include 'database.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous" defer></script>
+    <title>Health tech</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
+    <div class="container">
+      <a class="navbar-brand" href="patient_state.php">Our health</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="patient_state.php">Results</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Give feedback</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="feedback.php">Feedbacks</a>
+          </li>
+        </ul>
+      </div>
+  </div>
+</nav>
+
+<main>
+  <div class="container d-flex flex-column align-items-center">
